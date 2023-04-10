114c7d9ead09e7fce137801814931da34b911252
Janvier Nahimana <jnahimana@objectivity.co.uk>
2023-04-10 12:37:47 +0200

    stories: add edit add and new todo stories

--- src/stories/add-todo.stories.jsx
+++ src/stories/add-todo.stories.jsx
@@ -1,29 +1,52 @@
-import { AddTodo } from "pages/AddTodo";
-import { Router } from "react-router-dom";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { DataSchema } from "utils/validations";
 
+import moment from "moment";
+import AddTodo from "./components/AddTodo";
 
 export default {
-    title:"Forms/AddTodo",
-    component: AddTodo,
-    parameters:{
-        controls: {hideNoControlsWarning: true}
-    },
-    decorators:[
-        (Story)=>(
-            <Router>
-                <Story/>
-            </Router>
-        )
-    ]
+  title: "Forms/AddTodo",
+  argTypes: { onClick: { action: "onClick" } },
+  component: AddTodo,
+};
+
+const Template = (args) => <AddTodo {...args} />;
+
+export const NewTodo = Template.bind({});
+NewTodo.args={
+    dueDate:moment().format("yyy-MM-dd"),
+    name:"New Task",
+    priority:"Medium",
+    text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima corporis labore totam quos quibusdam maxime laborum nihil molestia"
 }
 
-export const Template = args => <AddTodo {...args} />
+export const EmptyTodo = Template.bind({});
+EmptyTodo.args={
+    dueDate:"",
+    name:"",
+    priority:"",
+    text:""
+}
 
-// export const NewTodo = Template.bind({})
+export const HighPriorityTodo = Template.bind({});
+HighPriorityTodo.args={
+    dueDate:moment().format("yyy-MM-dd"),
+    name:"New Task",
+    priority:"High",
+    text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima corporis labore totam quos quibusdam maxime laborum nihil molestia"
+}
 
-// NewTodo.args = {
-//     resolver:zodResolver(DataSchema),
-//     onSubmit:(data)=> console.log(data)
-// }
+export const MediumPriorityTodo = Template.bind({});
+MediumPriorityTodo.args={
+    dueDate:moment().format("yyy-MM-dd"),
+    name:"New Task",
+    priority:"Medium",
+    text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima corporis labore totam quos quibusdam maxime laborum nihil molestia"
+}
+
+export const LowPriorityTodo = Template.bind({});
+LowPriorityTodo.args={
+    dueDate:moment().format("yyy-MM-dd"),
+    name:"New Task",
+    priority:"Low",
+    text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima corporis labore totam quos quibusdam maxime laborum nihil molestia"
+}
+
--- src/stories/components/AddTodo.jsx
+++ src/stories/components/AddTodo.jsx
@@ -1,90 +1,93 @@
 import { Priority } from "models/Todo";
+import "../../index.css";
 
-const AddTodo = ({ onClick }) => {
+const AddTodo = ({ onClick, dueDate, name, priority, text }) => {
   return (
-    <>
-      <form onSubmit={onClick}>
-        <div className="shadow hover:shadow-md w-96 py-6 px-10">
-          <div className="space-y-6">
-            <h5 className="text-xl font-medium text-gray-900 text-blue-700">
-              New Task Detail
-            </h5>
-            <div className="col-span-6 sm:col-span-3">
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Name
-              </label>
-              <input className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
-              <small className=" text-red-700 font-medium">
-                {errors.name?.message}
-              </small>
-            </div>
-
-            <div className="col-span-6 sm:col-span-3">
-              <label
-                htmlFor="dueDate"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Due Date
-              </label>
-              <input
-                type="date"
-                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              />
-              <small className=" text-red-700 font-medium"> </small>
-            </div>
-
-            <small className=" text-red-700 font-medium"> </small>
-
-            <div className="col-span-6 sm:col-span-3">
-              <label
-                htmlFor="priority"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Priority
-              </label>
-              <select
-                id="priority"
-                autoComplete="priority"
-                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              >
-                {Object.keys(Priority).map((key, index) => (
-                  <option value={key} key={index}>
-                    {" "}
-                    {key}{" "}
-                  </option>
-                ))}
-              </select>
-              <small className=" text-red-700 font-medium"> </small>
-            </div>
-            <div className="col-span-6 sm:col-span-3">
-              <label
-                htmlFor="message"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                text
-              </label>
-              <textarea
-                id="message"
-                rows={4}
-                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Write your thoughts here..."
-              ></textarea>
-              <small className=" text-red-700 font-medium"> </small>
-            </div>
-
-            <button
-              type="submit"
-              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    <form>
+      <div className="shadow hover:shadow-md w-96 py-6 px-10">
+        <div className="space-y-6">
+          <h5 className="text-xl font-medium text-gray-900 text-blue-700">
+            New Task Detail
+          </h5>
+          <div className="col-span-6 sm:col-span-3">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
             >
-              Create Todo
-            </button>
+              Name
+            </label>
+            <input
+              value={name}
+              name="name"
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+            <small className=" text-red-700 font-medium"></small>
           </div>
+
+          <div className="col-span-6 sm:col-span-3">
+            <label
+              htmlFor="dueDate"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Due Date
+            </label>
+            <input
+              name="dueDate"
+              defaultValue={dueDate}
+              type="date"
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+            <small className=" text-red-700 font-medium"> </small>
+          </div>
+
+          <small className=" text-red-700 font-medium"> </small>
+
+          <div className="col-span-6 sm:col-span-3">
+            <label
+              htmlFor="priority"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Priority
+            </label>
+            <select
+              id="priority"
+              autoComplete="priority"
+              defaultValue={priority}
+              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            >
+              <option value="High">High</option>
+              <option value="Medium">Medium</option>
+              <option value="Low">Low</option>
+            </select>
+            <small className=" text-red-700 font-medium"> </small>
+          </div>
+          <div className="col-span-6 sm:col-span-3">
+            <label
+              htmlFor="message"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              text
+            </label>
+            <textarea
+              id="message"
+              rows={4}
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Write your thoughts here..."
+            >
+              {text}
+            </textarea>
+            <small className=" text-red-700 font-medium"> </small>
+          </div>
+
+          <button
+            onClick={onClick}
+            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Create Todo
+          </button>
         </div>
-      </form>
-    </>
+      </div>
+    </form>
   );
 };
 
